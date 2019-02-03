f8f5d943f4ed979f29bbf2a568c5a7c0a73b092a
Carlosc23 <carlos@inegua.com>
2019-02-02 22:11:05 -0600

    feat: add initial menu

--- Tarea1.py
+++ Tarea1.py
@@ -3,4 +3,22 @@
 # Tarea 1
 
 
-
+def menu():
+    return '---¡Bienvenido a la tarea 1 de graficas por computadora!---\n\nPor favor con el teclado ingrese el número' \
+           ' de acuerdo a la instrucción que desea ver, Una imagen con el resultado sera guardada en la ubicación ' \
+           'de este proyecto \n\n' \
+           '-----------------------------------------------------------------------------------------------------' \
+           '-------------------------------\n' \
+           '1. Por renderizar <una imagen negra con un punto blanco en una ubicación random dentro de la imagen.\n' \
+           '2. Por renderizar una imagen negra con un punto blanco en cada esquina\n' \
+           '3. Por renderizar un cubo de 100 pixeles en el centro de su imagen\n' \
+           '4. Por renderizar líneas blancas en toda la orilla de su imagen (4 lineas) \n' \
+           '5. Por renderizar una línea blanca en diagonal por el centro de su imagen. \n' \
+           '6. Por llenar su imagen entera de puntos blancos y negros ' \
+           '(las posibilidades de que un punto sea blanco o negro son de 50%)\n' \
+           '7. Por llenar su imagen entera de puntos de colores random \n' \
+           '8. Por crear una escena de un cielo con estrellas \n' \
+           '9. Por crear una escena de 160 x 192 pixeles representando un frame de un juego de Atari.(Pong)\n' \
+           '10. Para salir'  \
+           '-----------------------------------------------------------------------------------------------------' \
+           '-------------------------------\n' \
